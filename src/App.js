8e9b278cfebdeeb8039561cdbf727dffdec71df8
wimdenherder <wimdenherder@gmail.com>
2022-05-23 10:55:01 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import './App.css';
-import { useState, useEffect } from 'react';
-import ZapPlayer from './components/ZapPlayer';
-import ReactPlayer from 'react-player';
-import program from './assets/program.json';
-import { useSearchParams } from 'react-router-dom';
+import "./App.css";
+import { useState, useEffect } from "react";
+import ZapPlayer from "./components/ZapPlayer";
+import ReactPlayer from "react-player";
+import program from "./assets/program.json";
+import { useSearchParams } from "react-router-dom";
 
 function App() {
-  const bufferSize = 5;
-  let [searchParams, setSearchParams] = useSearchParams();
-  const random = searchParams.get('startIndex') || Math.floor(Math.random() * (program.length - bufferSize));
-  const selectionVideos = program.slice(random,random+bufferSize);
-  console.log('random', random);
-  useEffect(() => {
-    // setSearchParams({ startIndex: random });
-  },[])
-  
   return (
     <>
-     <ZapPlayer startIndex={random} program={selectionVideos}/>
+      <ZapPlayer startIndex={0} program={program} />
     </>
   );
 }
